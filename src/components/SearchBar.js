14c45ec5a5eb1a3ed50f5bdd5d38c7110f18c219
Chantelle Belic <chantelle.belic@gmail.com>
2018-12-10 18:48:24 -0800

    changed searchbar to func component bc i moved state to app

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,31 +4,24 @@ import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './SearchBar.css';
 
-class SearchBar extends Component {
+const SearchBar = (props) => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  onSearch = (event) => {
+  const onSearch = (event) => {
     // console.log(event.target.value)
-    this.props.searchPetCallback(event.target.value)
+    props.searchPetCallback(event.target.value)
   }
 
-  render() {
+
     return (
       <section>
         <form  className="search-bar-form">
           <h3>Search By Field</h3>
-          { /* A form should go here! */ }
           <label htmlFor="searchWord">Search a Pet!</label>
-          <input name="searchWord" type="text" value={this.state.fullName} onChange={this.onSearch}/>
+          <input name="searchWord" type="text" onChange={onSearch}/>
         </form>
       </section>
     );
-  }
+
 };
 
 SearchBar.propTypes = {
